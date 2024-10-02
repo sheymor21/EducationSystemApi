274427d2e92e1f496f345c18ad01aeae432b74dd
Sheymor <joseacvz81@gmail.com>
2024-10-02 16:48:23 -0400

    Add DTOs for Teacher and Student
    Introduced new data transfer objects (DTOs) for teachers and students in `teacherDto.go` and `studentDto.go` respectively. These DTOs include attributes such as Carnet, FirstName, LastName, Age, and Classroom with validation rules. This will streamline data handling and validation for teacher and student entities.

---
+++ internal/dto/studentDto.go
@@ -0,0 +1,9 @@
+package dto
+
+type StudentAddDto struct {
+	Carnet    string `json:"carnet" validate:"required,min=10,max=10"`
+	FirstName string `json:"firstName" validate:"required,min=3,max=20"`
+	LastName  string `json:"lastName" validate:"required,min=3,max=20"`
+	Age       uint8  `json:"age" validate:"required,min=1,max=100"`
+	Classroom string `json:"classroom" validate:"required,min=2,max=4"`
+}
