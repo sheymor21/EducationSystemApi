b7e75bbd6ed7400262ba4d099cec00436ed3efc5
Sheymor <joseacvz81@gmail.com>
2024-10-09 16:46:28 -0400

    Refactor student services to simplify concurrency logic
    Removed unnecessary goroutines and wait groups to streamline the flow of student services. Introduced a more straightforward handling mechanism for checking student existence using channels, and added a new getStudents function to fetch all students.

--- internal/services/studentServices.go
+++ internal/services/studentServices.go
@@ -16,101 +16,104 @@ import (
 )
 
 func addStudent(w http.ResponseWriter, r *http.Request) {
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		studentDto := dto.StudentAddDto{}
-		err := utilities.ReadJson(w, r, &studentDto)
-		if err != nil {
-			httpInternalError(w, err.Error())
-			log.Println(err)
-			return
-		}
-		student := models.Student{
-			Carnet:    studentDto.Carnet,
-			FirstName: studentDto.FirstName,
-			LastName:  studentDto.LastName,
-			Age:       studentDto.Age,
-			Classroom: studentDto.Classroom,
-		}
-		_, err = dbContext.Student.InsertOne(context.TODO(), student)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}()
-	wg.Wait()
+	studentDto := dto.StudentAddDto{}
+	err := utilities.ReadJson(w, r, &studentDto)
+	if err != nil {
+		httpInternalError(w, err.Error())
+		log.Println(err)
+		return
+	}
+	student := models.Student{
+		Carnet:    studentDto.Carnet,
+		FirstName: studentDto.FirstName,
+		LastName:  studentDto.LastName,
+		Age:       studentDto.Age,
+		Classroom: studentDto.Classroom,
+	}
+	_, err = dbContext.Student.InsertOne(context.TODO(), student)
+	if err != nil {
+		log.Println(err)
+		httpInternalError(w, err.Error())
+	}
+}
+
+func getStudents(w http.ResponseWriter, r *http.Request) {
+	var student []models.Student
+	find, findErr := dbContext.Student.Find(context.TODO(), bson.M{})
+	if findErr != nil {
+		log.Println(findErr)
+		httpInternalError(w, findErr.Error())
+		return
+	}
+	decodeErr := find.All(context.TODO(), &student)
+	if decodeErr != nil {
+		log.Println(decodeErr)
+		httpInternalError(w, decodeErr.Error())
+		return
+	}
+	utilities.WriteJson(w, http.StatusOK, student)
 }
 
 func getStudent(w http.ResponseWriter, r *http.Request) {
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		carnet := r.URL.Query().Get("Carnet")
-		student, err := getStudentByCarnet(dbContext, carnet)
-		if err != nil {
-			httpNotFoundError(w, customErrors.NewNotFoundMongoError("carnet").Msg)
-		} else {
+	carnet := r.URL.Query().Get("Carnet")
+	student, err := getStudentByCarnet(dbContext, carnet)
+	if err != nil {
+		httpNotFoundError(w, customErrors.NewNotFoundMongoError("carnet").Msg)
+	} else {
 
-			utilities.WriteJson(w, http.StatusOK, student)
-		}
-	}()
-	wg.Wait()
+		utilities.WriteJson(w, http.StatusOK, student)
+	}
 }
 
 func putStudent(w http.ResponseWriter, r *http.Request) {
 	var wg sync.WaitGroup
+	ch := make(chan bool)
+	carnet := r.URL.Query().Get("Carnet")
 	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		carnet := r.URL.Query().Get("Carnet")
-		anyStudent := anyStudent(carnet)
-		if !anyStudent {
-			httpNotFoundError(w, customErrors.NewNotFoundMongoError("carnet").Msg)
-		} else {
-
-			var student models.Student
-			err := utilities.ReadJson(w, r, &student)
-			if err != nil {
-				httpInternalError(w, err.Error())
-				log.Println(err)
-				return
-			}
-			filter := bson.D{{"carnet", carnet}}
-			update := bson.D{{"$set", student}}
-			_, err = dbContext.Student.UpdateOne(context.TODO(), filter, update)
-			if err != nil {
-				httpInternalError(w, err.Error())
-				log.Fatal(err)
-			}
-		}
-	}()
+	go anyStudent(carnet, &wg, ch)
+	if !<-ch {
+		httpNotFoundError(w, customErrors.NewNotFoundMongoError("carnet").Msg)
+		return
+	}
 	wg.Wait()
+
+	var student models.Student
+	student.Carnet = carnet
+	err := utilities.ReadJson(w, r, &student)
+	if err != nil {
+		httpInternalError(w, err.Error())
+		log.Println(err)
+		return
+	}
+	filter := bson.D{{"carnet", carnet}}
+	update := bson.D{{"$set", student}}
+	_, err = dbContext.Student.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		log.Println(err)
+		httpInternalError(w, err.Error())
+	}
 
 }
 
 func deleteStudent(w http.ResponseWriter, r *http.Request) {
 	var wg sync.WaitGroup
+	ch := make(chan bool)
+	carnet := r.URL.Query().Get("Carnet")
 	wg.Add(1)
-	go func() {
-		defer wg.Done()
-
-		carnet := r.URL.Query().Get("Carnet")
-		anyStudent := anyStudent(carnet)
-		if !anyStudent {
-			httpNotFoundError(w, customErrors.NewNotFoundMongoError("carnet").Msg)
-		} else {
-
-			filter := bson.D{{"carnet", carnet}}
-			_, err := dbContext.Student.DeleteOne(context.TODO(), filter)
-			if err != nil {
-				log.Fatal(err)
-				return
-			}
-		}
-	}()
+	go anyStudent(carnet, &wg, ch)
+	if <-ch {
+		httpNotFoundError(w, customErrors.NewNotFoundMongoError("carnet").Msg)
+		return
+	}
 	wg.Wait()
+
+	filter := bson.D{{"carnet", carnet}}
+	_, err := dbContext.Student.DeleteOne(context.TODO(), filter)
+	if err != nil {
+		log.Println(err)
+		httpInternalError(w, err.Error())
+		return
+	}
 }
 
 func getStudentByCarnet(dbContext *database.MongoContext, carnet string) (models.Student, *customErrors.NotFoundMongoError) {
@@ -124,17 +127,17 @@ func getStudentByCarnet(dbContext *database.MongoContext, carnet string) (models
 	return student, nil
 }
 
-func anyStudent(carnet string) bool {
-
+func anyStudent(carnet string, wg *sync.WaitGroup, ch chan bool) {
+	defer wg.Done()
+	defer close(ch)
 	filter := bson.D{{"carnet", carnet}}
 	err := dbContext.Student.FindOne(context.TODO(), filter).Decode(&models.Student{})
 	if errors.Is(err, mongo.ErrNoDocuments) {
-		return false
+		ch <- false
 	} else if err != nil {
-		log.Fatal(err)
-		return false
+		ch <- false
+		log.Println(err)
 	} else {
-
-		return true
+		ch <- true
 	}
 }
