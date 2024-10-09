d25edae43e29601638af220c21e8da82234f0ab9
Sheymor <joseacvz81@gmail.com>
2024-10-09 19:50:56 -0400

    Refactor async calls and error handling in teacher services
    Removed unnecessary goroutines and wait groups to simplify the code in `updateTeacher`, `deleteTeacher`, and `getTeacher` functions. Enhanced error handling patterns and optimized the `anyTeacher` function to use channels for better concurrency management.

--- internal/services/teacherServices.go
+++ internal/services/teacherServices.go
@@ -28,65 +28,52 @@ func addTeacher(w http.ResponseWriter, r *http.Request) {
 }
 
 func updateTeacher(w http.ResponseWriter, r *http.Request) {
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		var teacher models.Teacher
-		err := utilities.ReadJson(w, r, &teacher)
-		if err != nil {
-			httpInternalError(w, err.Error())
-			log.Println(err)
-			return
-		}
-		filter := bson.M{"carnet": teacher.Carnet}
-		update := bson.M{"$set": teacher}
-		_, err = dbContext.Teachers.UpdateOne(context.TODO(), filter, update)
-		if err != nil {
-			httpInternalError(w, err.Error())
-		}
-	}()
-	wg.Wait()
+	var teacher models.Teacher
+	err := utilities.ReadJson(w, r, &teacher)
+	if err != nil {
+		httpInternalError(w, err.Error())
+		log.Println(err)
+		return
+	}
+	filter := bson.M{"carnet": teacher.Carnet}
+	update := bson.M{"$set": teacher}
+	_, err = dbContext.Teachers.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		httpInternalError(w, err.Error())
+		return
+	}
 }
 
 func deleteTeacher(w http.ResponseWriter, r *http.Request) {
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		carnet := r.URL.Query().Get("Carnet")
-		filter := bson.M{"carnet": carnet}
+	carnet := r.URL.Query().Get("Carnet")
+	filter := bson.M{"carnet": carnet}
 
-		_, err := dbContext.Teachers.DeleteOne(context.TODO(), filter)
-		if err != nil {
-			httpInternalError(w, err.Error())
-			return
-		}
-	}()
-	wg.Wait()
+	_, err := dbContext.Teachers.DeleteOne(context.TODO(), filter)
+	if err != nil {
+		httpInternalError(w, err.Error())
+		return
+	}
 
 }
 
 func getTeacher(w http.ResponseWriter, r *http.Request) {
 	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		carnet := r.URL.Query().Get("Carnet")
-		anyTeacher := anyTeacher(carnet)
-		if !anyTeacher {
-			httpNotFoundError(w, customErrors.NewNotFoundMongoError("carnet").Error())
-		} else {
-			var teacher models.Teacher
-			filter := bson.M{"carnet": carnet}
-			err := dbContext.Teachers.FindOne(context.TODO(), filter).Decode(&teacher)
-			if err != nil {
-				httpNotFoundError(w, customErrors.NewNotFoundMongoError("carnet").Error())
-			}
-			utilities.WriteJson(w, http.StatusOK, teacher)
-		}
-	}()
+	ch := make(chan bool)
+	carnet := r.URL.Query().Get("Carnet")
+	go anyTeacher(carnet, &wg, ch)
+	if <-ch {
+		httpNotFoundError(w, customErrors.NewNotFoundMongoError("carnet").Error())
+		return
+	}
 	wg.Wait()
+	var teacher models.Teacher
+	filter := bson.M{"carnet": carnet}
+	err := dbContext.Teachers.FindOne(context.TODO(), filter).Decode(&teacher)
+	if err != nil {
+		httpNotFoundError(w, customErrors.NewNotFoundMongoError("carnet").Error())
+		return
+	}
+	utilities.WriteJson(w, http.StatusOK, teacher)
 
 }
 
@@ -94,27 +81,27 @@ func getTeachers(w http.ResponseWriter, r *http.Request) {
 	var teachers []models.Teacher
 	find, findErr := dbContext.Teachers.Find(context.TODO(), bson.M{})
 	if findErr != nil {
-		log.Fatal(findErr)
+		log.Println(findErr)
 		return
 	}
 	decodeErr := find.All(context.TODO(), &teachers)
 	if decodeErr != nil {
-		log.Fatal(decodeErr)
+		log.Println(decodeErr)
 		return
 	}
 	utilities.WriteJson(w, http.StatusOK, teachers)
 }
-func anyTeacher(carnet string) bool {
-
+func anyTeacher(carnet string, wg *sync.WaitGroup, ch chan bool) {
+	defer wg.Done()
+	defer close(ch)
 	filter := bson.D{{"carnet", carnet}}
 	err := dbContext.Teachers.FindOne(context.TODO(), filter).Decode(&models.Teacher{})
 	if errors.Is(err, mongo.ErrNoDocuments) {
-		return false
+		ch <- false
 	} else if err != nil {
-		log.Fatal(err)
-		return false
+		log.Println(err)
+		ch <- false
 	} else {
-
-		return true
+		ch <- true
 	}
 }
