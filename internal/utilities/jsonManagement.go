b4765d55dea0d4bb7320210078fe3be9994679d6
Sheymor <joseacvz81@gmail.com>
2024-10-09 19:42:28 -0400

    Change Fatal to Println for JSON Write errors
    Switching from log.Fatal to log.Println ensures the server continues running even when JSON marshaling or writing fails. This enhances error handling without shutting down the application abruptly.

--- internal/utilities/jsonManagement.go
+++ internal/utilities/jsonManagement.go
@@ -49,14 +49,14 @@ func ReadJson(w http.ResponseWriter, r *http.Request, mapper any) error {
 func WriteJson(w http.ResponseWriter, status int, data any) {
 	indent, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 		return
 	}
 	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
 	w.WriteHeader(status)
 	_, err = w.Write(indent)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 		return
 	}
 }
