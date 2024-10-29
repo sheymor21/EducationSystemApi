6cd86ca017a2bb9a670d2e2b98563dcf2c3424ed
Sheymor <joseacvz81@gmail.com>
2024-10-29 14:48:54 -0400

    Fix, adjust hash cost while making the password

--- internal/services/userServices.go
+++ internal/services/userServices.go
@@ -8,12 +8,13 @@ import (
 	"fmt"
 	"go.mongodb.org/mongo-driver/bson"
 	"golang.org/x/crypto/bcrypt"
+	"strings"
 )
 
 func addUser(firstName, lastname, carnet string, rol validations.Rol) error {
 	userName := fmt.Sprintf("%s %s", firstName, lastname)
-	password := fmt.Sprintf("%s-%s", carnet[len(carnet)-3:], lastname)
-	hashPassword, hashErr := bcrypt.GenerateFromPassword([]byte(password), 14)
+	password := fmt.Sprintf("%s-%s", carnet[len(carnet)-3:], strings.ToLower(lastname))
+	hashPassword, hashErr := bcrypt.GenerateFromPassword([]byte(password), 4)
 	if hashErr != nil {
 		utilities.Log.Errorln(hashErr)
 		return hashErr
